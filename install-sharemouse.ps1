9c311b22e847b70d963e320e4adb696782d33fa7
Intrestang <133173615+Intrestang@users.noreply.github.com>
2024-10-29 21:30:55 +1100

    Update install-sharemouse.ps1

--- install-sharemouse.ps1
+++ install-sharemouse.ps1
@@ -14,6 +14,11 @@ if (Test-Path -Path $dest) {
     Start-Process -FilePath $dest -ArgumentList "/silent" -Wait
 
     Write-Output "Installation complete."
+
+    # Delete the scheduled task used to run this script with elevated privileges
+    Write-Output "Cleaning up scheduled task..."
+    schtasks /delete /tn InstallShareMouse /f
+    Write-Output "Scheduled task deleted."
 } else {
     Write-Output "Download failed. Please check the URL or network connection."
 }
